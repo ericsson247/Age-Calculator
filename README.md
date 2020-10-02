b599bdfa48979a8c2b06169e0375e557953a99df
Eric Yohanna Mshelmbula <ericyohanna247@gmail.com>
2020-10-02 17:29:39 +0100

    ageCalculator

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Age-Calculator
-The Application is designed to calculate number of days you lived on earth
+# Project Name: Age-Calculator
+## About Me
+My name is Eric Yohanna Mshelmbula, I am from Garkida Gombi LGA Adamawa State, Nigeria. You can connect to me Via my social media handles; Twitter: @Emshelmbula, Facebook: Eric Yohanna.
+## About the Project
+The project is designed to calculate the days you lived on earth.
+## Features
+The project features requires your first name, surname and age.
+## Tool used 
+Visual Studio Code
+## Developed with;
+CSS, HTML and JavaScript
+# Image
+![Age_Calculator](ageCalculator.png)
--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 const agebutton1 = document.querySelector('.agebutton');
 const box2 = document.querySelector('.box2');
 const box1 = document.querySelector('.box1');
-const summit = document.querySelector('[type = "submit"]');
+const summit = document.querySelector('.summit');
 const box3 = document.querySelector('.box3');
 
-agebutton1.addEventListener('.agebutton', function(){
-box2.classList.remove('hidden');
-box1.classList.add('hidden');
+agebutton1.addEventListener('click', function(){
+    box2.classList.remove('hidden');
+    box1.classList.add('hidden');
 })
 
+summit.addEventListener('click', function(e){
+    e.preventDefault();
+
+    var fName, sName, age;
+    const displayAge = document.querySelector('#output');
+
+    fName = document.getElementById("aName").value;
+    sName = document.getElementById("bName").value;
+    age = document.getElementById("cAge").value; 
+
+    if (fName === " " || sName === " " || age === ""){
+        alert("Please, fill in your details");
+        return;
+    }else{
+        displayAge.value = `${age * 365} days`;
+    }
+    box2.classList.add('hidden');
+    box1.classList.add('hidden');
+    box3.classList.remove('hidden');
+})
